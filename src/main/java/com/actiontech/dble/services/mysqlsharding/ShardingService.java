5fb7d7b37ce3393f31ea2d5a589edaa23a27a753
lin <yuanlinzhu1995@gmail.com>
2021-04-26 17:19:16 +0800

    fix begin sql not set autocommit field to false inner 801
    support session read only (#2607)
    
    * support session read only
    
    * remove useless import
    
    fix bug that "set autocommit=1" doesn't work,eg set autocommit=0;set autocommit=1;dml;rollback; dml is rollback (#2624)
    
    inner-690: fix AsynchronousCloseException while read (#2619)
    
    Signed-off-by: dcy <dcy10000@gmail.com>
    
    fix support  like database.table.Column insert inner 905 (#2613)
    
    add check database and table
    
    add check database and table
    
    fix support  like database.table.Column insert inner 905
    
    add check database and table
    
    add check
    
    add check
    
    delete code
    
    test file
    
    fffff

--- src/main/java/com/actiontech/dble/services/mysqlsharding/ShardingService.java
+++ src/main/java/com/actiontech/dble/services/mysqlsharding/ShardingService.java
@@ -1,7 +1,6 @@
 package com.actiontech.dble.services.mysqlsharding;
 
 import com.actiontech.dble.DbleServer;
-import com.actiontech.dble.backend.mysql.VersionUtil;
 import com.actiontech.dble.backend.mysql.nio.handler.transaction.savepoint.SavePointHandler;
 import com.actiontech.dble.config.ErrorCode;
 import com.actiontech.dble.config.model.SystemConfig;
@@ -77,7 +76,6 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
     private volatile boolean isLocked = false;
     private long lastInsertId;
     private final NonBlockingSession session;
-    private boolean sessionReadOnly = false;
     private ServerSptPrepare sptprepare;
     private volatile RequestScope requestScope;
     protected volatile boolean setNoAutoCommit = false;
@@ -111,24 +109,22 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
                 session.setTrace(Boolean.parseBoolean(val));
                 this.singleTransactionsCount();
                 break;
-            case TX_READ_ONLY:
-                sessionReadOnly = Boolean.parseBoolean(val);
-                this.singleTransactionsCount();
-                break;
             case AUTOCOMMIT:
                 if (Boolean.parseBoolean(val)) {
                     if (!autocommit) {
                         Optional.ofNullable(StatisticListener.getInstance().getRecorder(this)).ifPresent(r -> r.onTxEnd());
-                    }
-                    if (!autocommit && session.getTargetCount() > 0) {
-                        setNoAutoCommit = true;
-                        session.implicitCommit(() -> {
-                            autocommit = true;
+                        if (session.getTargetCount() > 0) {
+                            setNoAutoCommit = true;
+                            session.implicitCommit(() -> {
+                                autocommit = true;
+                                txStarted = false;
+                                this.singleTransactionsCount();
+                                writeOkPacket();
+                            });
+                            return;
+                        } else if (txStarted) {
                             txStarted = false;
-                            this.singleTransactionsCount();
-                            writeOkPacket();
-                        });
-                        return;
+                        }
                     }
                     autocommit = true;
                 } else {
@@ -154,8 +150,6 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
         List<MysqlVariable> variables = super.getAllVars();
         variables.add(new MysqlVariable("xa", session.getTransactionManager().getSessionXaID() == null ? "false" : "true", VariableType.SYSTEM_VARIABLES));
         variables.add(new MysqlVariable("trace", session.isTrace() + "", VariableType.SYSTEM_VARIABLES));
-        variables.add(new MysqlVariable(VersionUtil.TRANSACTION_READ_ONLY, sessionReadOnly + "", VariableType.SYSTEM_VARIABLES));
-        variables.add(new MysqlVariable(VersionUtil.TX_READ_ONLY, sessionReadOnly + "", VariableType.SYSTEM_VARIABLES));
         return variables;
     }
 
@@ -199,8 +193,6 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
         SerializableLock.getInstance().lock(this.connection.getId());
 
         this.handler.setReadOnly(userConfig.isReadOnly());
-
-        this.handler.setSessionReadOnly(sessionReadOnly);
         this.handler.query(sql);
     }
 
@@ -680,24 +672,12 @@ public class ShardingService extends BusinessService<ShardingUserConfig> {
         this.lastInsertId = lastInsertId;
     }
 
-    public void setSessionReadOnly(boolean sessionReadOnly) {
-        this.sessionReadOnly = sessionReadOnly;
-    }
-
-    public boolean isReadOnly() {
-        return sessionReadOnly;
-    }
-
     public ServerLoadDataInfileHandler getLoadDataInfileHandler() {
         return loadDataInfileHandler;
     }
 
     public ServerSptPrepare getSptPrepare() {
         return sptprepare;
-    }
-
-    private boolean isEndOfDataFile(byte[] data) {
-        return (data.length == 4 && data[0] == 0 && data[1] == 0 && data[2] == 0);
     }
 
     public boolean isSetNoAutoCommit() {
